19ec6f628c6c91c7288efa9b12c6da0eeeedee64
arelanno <a.relano@alumnos.urjc.es>
2019-10-28 01:58:05 +0100

    modificaciones del apartado 7 hechas en server.py

--- server.py
+++ server.py
@@ -8,26 +8,35 @@ import socketserver
 import sys
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    dicc_register = {}
+ 
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
         self.wfile.write(b"Hemos recibido tu peticion")
+        info_user= {}
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
-            print(self.client_address)
+            if line.decode('utf-8') != "\r\n":
+                print("El cliente nos manda ", line.decode('utf-8'))
+             ## print(self.client_address)
+                text_rec = line.decode('utf-8').split(" ")
+                if text_rec[0] == "REGISTER":
+                    user = text_rec[1]
+                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                    info_user["address"] = self.client_address[0]
+                    self.dicc_register[user] = info_user
 
 if __name__ == "__main__":
     Port = int(sys.argv[1])
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', Port), EchoHandler) 
+    serv = socketserver.UDPServer(('', Port), SIPRegisterHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
