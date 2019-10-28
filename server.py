95f61641b1b7af2adf3bdeeb6d845c2df1da7fa5
arelanno <a.relano@alumnos.urjc.es>
2019-10-28 18:40:29 +0100

    apartado 11 hecho en server.py

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     OK reply server class
     """
     dicc_register = {}
- 
+
     def handle(self):
         """
         handle method of the server class
@@ -24,7 +24,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         self.wfile.write(b"Hemos recibido tu peticion ")
         self.json2registered()
 
-        info_user= {}
+        info_user = {}
 
         for line in self.rfile:
             if line.decode('utf-8') != "\r\n":
@@ -35,7 +35,6 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                     user = text_rec[1]
                     self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                     info_user["address"] = self.client_address[0]
-                   # info_user["expires_value"] = expires_value
                     self.dicc_register[user] = info_user
                 if text_rec[3] == "Expires:":
                     if expires_value == 0 and user in self.dicc_register:
@@ -44,13 +43,13 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                     else:
                         expires_date = time.strftime('%Y-%m-%d %H:%M:%S',
-                                      time.gmtime(time.time() + expires_value))
+                        time.gmtime(time.time() + expires_value))
                         info_user["expires_value"] = expires_date
         self.registered2json()
 
     def registered2json(self):
 
-        file = open("registered.json", 'w') 
+        file = open("registered.json", 'w')
         json.dump(self.dicc_register, file)
 
     def json2registered(self):
@@ -65,7 +64,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
 if __name__ == "__main__":
     Port = int(sys.argv[1])
-    # Listens at localhost ('') port 6001 
+    # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
     serv = socketserver.UDPServer(('', Port), SIPRegisterHandler)
 
