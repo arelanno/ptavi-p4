2adea9a98deb30f8fbdb04408caa67ee025e6bc8
arelanno <a.relano@alumnos.urjc.es>
2019-10-28 04:45:43 +0100

    apartado 8 en server.py

--- server.py
+++ server.py
@@ -21,16 +21,23 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         """
         self.wfile.write(b"Hemos recibido tu peticion")
         info_user= {}
+
         for line in self.rfile:
             if line.decode('utf-8') != "\r\n":
                 print("El cliente nos manda ", line.decode('utf-8'))
              ## print(self.client_address)
                 text_rec = line.decode('utf-8').split(" ")
+                expires_value = int(text_rec[4])
                 if text_rec[0] == "REGISTER":
                     user = text_rec[1]
                     self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                     info_user["address"] = self.client_address[0]
+                    info_user["expires_value"] = expires_value
                     self.dicc_register[user] = info_user
+                if text_rec[3] == "Expires:" and expires_value == 0:
+                    del self.dicc_register[user]
+                    print("Borrado usuario " + user)
+                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     Port = int(sys.argv[1])
