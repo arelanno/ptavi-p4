ff4c31f8402d50ba5a42f4d4c7c54ab68789b680
arelanno <a.relano@alumnos.urjc.es>
2019-10-28 01:58:37 +0100

    modificaciones del apartado 7 hechas en client.py

--- client.py
+++ client.py
@@ -10,17 +10,24 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
+MType = sys.argv[3]
+User = sys.argv[4]
 LINE = ""
 
-for words in sys.argv[3:]:
-    LINE = LINE + " " + words
+if MType == 'register':
+    LINE = ('REGISTER sip:' + User + ' SIP/2.0\r\n')
+
+#for words in sys.argv[5:]:
+ #   LINE = LINE + " " + words
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.connect((SERVER, PORT))
+        print("Enviando:", LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        print('Recibido -- ', data.decode('utf-8'))
+else:
+    sys.exit('Usage: client.py ip puerto register sip_address')
 
 print("Socket terminado.")
